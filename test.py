5db86de25505196385bf0d6a97955c6504566bdb
Tal Tamir <tal.tamir@cyesec.com>
2020-02-09 14:42:32 +0200

    Added print command

--- test.py
+++ test.py
@@ -1,9 +1,7 @@
 import json
 import os
 
-VALID_LOCATIONS = ['Tel Aviv', 'Ramat Aviv', 'Ramat Gan', 'Giv`atayim', 'Bat Yam', 'H̱olon', 'Ramat HaSharon', 'Yehud', 'Yafo', 'Herzliyya', 'Kfar Saba', 'Nes Ziyyona', 'Qiryat Ono', 'Ra`ananna', 'Ramat H̱en',
-                   'Hod HaSharon', 'Gelilot', 'Ramat H̱ayyal', 'Rishon LeẔiyyon', 'Hadar Yosef', 'Ramat H̱en', 'Giv`at Shemu’el', 'Ezra Uviẕẕaron', 'Qiryat Shalom', 'Kefar Gannim', 'Gan H̱ayyim', 'Reẖovot', 'Petaẖ Tiqwa', 'Herzliyya']
-LOCAL_FILE_NAME = "users2.json"
+LOCAL_FILE_NAME = "users3.json"
 
 MIN_AGE = 24
 MAX_AGE = 47
@@ -31,8 +29,18 @@ class bumble:
         validDistance = user['distance'] <= MAX_DISTANCE
         return validKids and validAge and validDistance
 
+    def printUsers(self, validUsers, brokenUsers):
+        print("\n********* VALID USERS ********\n")
+        for user in validUsers:
+            print(user["name"] + " " + str(user["age"]) + " " +
+                  user["location"] + " " + str(user["distance"]) + user["aboutme_text"])
+        print("\n********* BROKEN USERS ********\n")
+        for user in brokenUsers:
+            print(user["name"] + " " + str(user["age"]) + " " +
+                  user["location"] + " " + str(user["distance"]) + user["aboutme_text"])
+
     def getUsers(self):
-        # command = '''curl 'https://bumble.com/mwebapi.phtml?SERVER_GET_ENCOUNTERS' -H 'Connection: keep-alive' -H 'Pragma: no-cache' -H 'Cache-Control: no-cache' -H 'User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.130 Safari/537.36' -H 'x-use-session-cookie: 1' -H 'Content-Type: json' -H 'Accept: */*' -H 'Origin: https://bumble.com' -H 'Sec-Fetch-Site: same-origin' -H 'Sec-Fetch-Mode: cors' -H 'Referer: https://bumble.com/app' -H 'Accept-Encoding: gzip, deflate, br' -H 'Accept-Language: en-US,en;q=0.9,he-IL;q=0.8,he;q=0.7' -H 'Cookie: session_cookie_name=session; device_id=58c2b8be-b8be-be15-1510-1045ebb25daa; aid=725246466; HDR-X-User-id=725246466; cookie_banner_closed=true; session=s1:30:txDLv83CBdSjsuFlXnIwDtYHVF6axKxXC6D2SQAM' --data-binary '{"body":[{"message_type":81,"server_get_encounters":{"number":10,"context":1,"user_field_filter":{"projection":[210,370,200,230,490,540,530,560,291,732,890,930,662,570,380,493,1140,1150,1160,1161],"request_albums":[{"album_type":7},{"album_type":12,"external_provider":12,"count":8}],"game_mode":0,"request_music_services":{"top_artists_limit":8,"supported_services":[29],"preview_image_size":{"width":120,"height":120}}}}}],"message_id":8,"message_type":81,"version":1,"is_background":false}' --compressed > ''' + LOCAL_FILE_NAME
+        # command = '''curl 'https://bumble.com/mwebapi.phtml?SERVER_GET_ENCOUNTERS' -H 'Connection: keep-alive' -H 'Pragma: no-cache' -H 'Cache-Control: no-cache' -H 'User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.130 Safari/537.36' -H 'x-use-session-cookie: 1' -H 'Content-Type: json' -H 'Accept: */*' -H 'Origin: https://bumble.com' -H 'Sec-Fetch-Site: same-origin' -H 'Sec-Fetch-Mode: cors' -H 'Referer: https://bumble.com/app' -H 'Accept-Encoding: gzip, deflate, br' -H 'Accept-Language: en-US,en;q=0.9,he-IL;q=0.8,he;q=0.7' -H 'Cookie: session_cookie_name=session; device_id=2a6ab11f-b11f-1fe1-e11d-1d4594af623f; cookie_banner_closed=true; aid=725246466; HDR-X-User-id=725246466; session=s1:30:vd8V1Huc0Swww2L809MMslyGiH2aG2PIYN5PgIC9' --data-binary '{"body":[{"message_type":81,"server_get_encounters":{"number":10,"context":1,"user_field_filter":{"projection":[210,370,200,230,490,540,530,560,291,732,890,930,662,570,380,493,1140,1150,1160,1161],"request_albums":[{"album_type":7},{"album_type":12,"external_provider":12,"count":8}],"game_mode":0,"request_music_services":{"top_artists_limit":8,"supported_services":[29],"preview_image_size":{"width":120,"height":120}}}}}],"message_id":8,"message_type":81,"version":1,"is_background":false}' --compressed > ''' + LOCAL_FILE_NAME
         # res = os.system(command)
 
         with open(LOCAL_FILE_NAME) as json_file:
@@ -66,7 +74,7 @@ class bumble:
                     newUser['summary_secondary'] = _user["profile_summary"]["secondary_text"]
 
             newUser['distance'] = 0.0
-            if "distance_short" in _user:
+            if "distance_short" in _user and _user["distance_short"] is not '':
                 newUser['distance'] = float(
                     _user["distance_short"].split(' ')[0].replace('~', ''))
 
@@ -81,10 +89,7 @@ class bumble:
             validUsers.append(newUser) if self.isValidUser(
                 newUser) else brokenUsers.append(newUser)
 
-        print("\n********* VALID USERS ********\n")
-        print(validUsers)
-        print("\n********* BROKEN USERS ********\n")
-        print(brokenUsers)
+        self.printUsers(validUsers, brokenUsers)
 
 
 b = bumble()
