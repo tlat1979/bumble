de2618c499c512df7be44c40df600a6fd21a5b5f
Tal Tamir <tal.tamir@cyesec.com>
2020-02-10 11:00:28 +0200

    like / pass are noe voteUser function

--- test.py
+++ test.py
@@ -7,31 +7,32 @@ LOCAL_FILE_NAME = "users3.json"
 LIKE_FILE_NAME = "likedUsers.json"
 PASS_FILE_NAME = "passedUsers.json"
 
+LIKE_VOTE = 2
+PASS_VOTE = 3
+
 MIN_AGE = 24
 MAX_AGE = 47
 
 MAX_DISTANCE = 15.0
-KIDS_PHRASES = [' ילד ', ' ילדה ', ' + ',
-                ' פלוס ', ' אמא ', ' נסיך ', ' נסיכה ']
-
+KIDS_PHRASES = [' ילד ', ' ילדה ', ' + ', ' פלוס ', ' אמא ', ' נסיך ', ' נסיכה ']
 
 class bumble:
 
-    def likeUser(self, user):
+    # Like vote: 2 = LIKE_VOTE  ////// Pass vote: 3 = PASS_VOTE
+    def voteUser(self, user, vote):  
         rand = randint(10, 20)
-        print("likeUser - Sleeping: "+str(rand)+" Seconds")
+        print("voteUser - Sleeping: "+str(rand)+" Seconds")
         time.sleep(rand)
-        command = '''curl 'https://bumble.com/mwebapi.phtml?SERVER_ENCOUNTERS_VOTE' -H 'Connection: keep-alive' -H 'Pragma: no-cache' -H 'Cache-Control: no-cache' -H 'User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.130 Safari/537.36' -H 'x-use-session-cookie: 1' -H 'Content-Type: json' -H 'Accept: */*' -H 'Origin: https://bumble.com' -H 'Sec-Fetch-Site: same-origin' -H 'Sec-Fetch-Mode: cors' -H 'Referer: https://bumble.com/app' -H 'Accept-Encoding: gzip, deflate, br' -H 'Accept-Language: en-US,en;q=0.9,he-IL;q=0.8,he;q=0.7' -H 'Cookie: session_cookie_name=session; device_id=2a6ab11f-b11f-1fe1-e11d-1d4594af623f; cookie_banner_closed=true; aid=725246466; HDR-X-User-id=725246466; session=s1:30:vd8V1Huc0Swww2L809MMslyGiH2aG2PIYN5PgIC9' --data-binary '{"body":[{"message_type":80,"server_encounters_vote":{"person_id":\"'''+user[
-            "user_id"]+'''\","vote":2,"vote_source":1,"game_mode":0}}],"message_id":13,"message_type":80,"version":1,"is_background":false}' --compressed >> ''' + LIKE_FILE_NAME
+
+        command = '''curl 'https://bumble.com/mwebapi.phtml?SERVER_ENCOUNTERS_VOTE' -H 'Connection: keep-alive' -H 'Pragma: no-cache' -H 'Cache-Control: no-cache' -H 'User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.130 Safari/537.36' -H 'x-use-session-cookie: 1' -H 'Content-Type: json' -H 'Accept: */*' -H 'Origin: https://bumble.com' -H 'Sec-Fetch-Site: same-origin' -H 'Sec-Fetch-Mode: cors' -H 'Referer: https://bumble.com/app' -H 'Accept-Encoding: gzip, deflate, br' -H 'Accept-Language: en-US,en;q=0.9,he-IL;q=0.8,he;q=0.7' -H 'Cookie: session_cookie_name=session; device_id=2a6ab11f-b11f-1fe1-e11d-1d4594af623f; cookie_banner_closed=true; aid=725246466; HDR-X-User-id=725246466; session=s1:30:vd8V1Huc0Swww2L809MMslyGiH2aG2PIYN5PgIC9' --data-binary '{"body":[{"message_type":80,"server_encounters_vote":{"person_id":\"'''+user["user_id"]+'''\","vote":'''+str(vote)+''',"vote_source":1,"game_mode":0}}],"message_id":13,"message_type":80,"version":1,"is_background":false}' --compressed >> ''' + LIKE_FILE_NAME
         #res = os.system(command)
 
-    def passUser(self, user):
-        rand = randint(10, 20)
-        print("passUser - Sleeping: "+str(rand)+" Seconds")
-        time.sleep(rand)
-        command = '''curl 'https://bumble.com/mwebapi.phtml?SERVER_ENCOUNTERS_VOTE' -H 'Connection: keep-alive' -H 'Pragma: no-cache' -H 'Cache-Control: no-cache' -H 'User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.130 Safari/537.36' -H 'x-use-session-cookie: 1' -H 'Content-Type: json' -H 'Accept: */*' -H 'Origin: https://bumble.com' -H 'Sec-Fetch-Site: same-origin' -H 'Sec-Fetch-Mode: cors' -H 'Referer: https://bumble.com/app' -H 'Accept-Encoding: gzip, deflate, br' -H 'Accept-Language: en-US,en;q=0.9,he-IL;q=0.8,he;q=0.7' -H 'Cookie: session_cookie_name=session; device_id=2a6ab11f-b11f-1fe1-e11d-1d4594af623f; cookie_banner_closed=true; aid=725246466; HDR-X-User-id=725246466; session=s1:30:vd8V1Huc0Swww2L809MMslyGiH2aG2PIYN5PgIC9' --data-binary '{"body":[{"message_type":80,"server_encounters_vote":{"person_id":\"'''+user[
-            "user_id"]+'''\","vote":3,"vote_source":1,"game_mode":0}}],"message_id":13,"message_type":80,"version":1,"is_background":false}' --compressed >> ''' + PASS_FILE_NAME
-        #res = os.system(command)
+        userDetails = user["name"] + " " + str(user["age"]) + " " + user["city"] + " " + str(user["distance"])
+        command2 = '''echo "'''+userDetails+'''" >> '''
+        if vote is LIKE_VOTE:
+            os.system(command2 + LIKE_FILE_NAME)
+        else:
+            os.system(command2 + PASS_FILE_NAME)
 
     def isValidUser(self, user):
         validKids = True
@@ -100,14 +101,13 @@ class bumble:
 
             newUser["age"] = int(newUser["age"])
 
-            validUsers.append(newUser) if self.isValidUser(
-                newUser) else brokenUsers.append(newUser)
+            validUsers.append(newUser) if self.isValidUser(newUser) else brokenUsers.append(newUser)
 
-            for user in validUsers:
-                self.likeUser(user)
+        for user in validUsers:
+            self.voteUser(user, LIKE_VOTE)
 
-            for user in brokenUsers:
-                self.passUser(user)
+        for user in brokenUsers:
+            self.voteUser(user, PASS_VOTE)
 
         self.printUsers(validUsers, brokenUsers)
 
