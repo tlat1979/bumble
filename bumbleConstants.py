be61bf4f2e1beb080657c8b9ff4f3ffff9365db0
Tal Tamir <tal.tamir@cyesec.com>
2020-02-10 14:35:04 +0200

    Added a contants file

---
+++ bumbleConstants.py
@@ -0,0 +1,27 @@
+from random import randint
+import json
+import time
+import os
+
+USERS_FILE_NAME = "users3.json"
+LIKE_FILE_NAME = "likedUsers.json"
+PASS_FILE_NAME = "passedUsers.json"
+
+BASE_BUMBLE_URL = '\'https://bumble.com/mwebapi.phtml?'
+GET_USERS_URL = BASE_BUMBLE_URL + 'SERVER_GET_ENCOUNTERS\''
+VOTE_USER_URL = BASE_BUMBLE_URL + 'SERVER_ENCOUNTERS_VOTE\''
+
+BASE_HEADERS = '''-H 'Connection: keep-alive' -H 'Pragma: no-cache' -H 'Cache-Control: no-cache' -H 'User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.130 Safari/537.36' -H 'x-use-session-cookie: 1' -H 'Content-Type: json' -H 'Accept: */*' -H 'Origin: https://bumble.com' -H 'Sec-Fetch-Site: same-origin' -H 'Sec-Fetch-Mode: cors' -H 'Referer: https://bumble.com/app' -H 'Accept-Encoding: gzip, deflate, br' -H 'Accept-Language: en-US,en;q=0.9,he-IL;q=0.8,he;q=0.7' -H 'Cookie: session_cookie_name=session; device_id=2a6ab11f-b11f-1fe1-e11d-1d4594af623f; cookie_banner_closed=true; aid=725246466; HDR-X-User-id=725246466; ''' 
+SESSION_ID = 'session=s1:30:vd8V1Huc0Swww2L809MMslyGiH2aG2PIYN5PgIC9'
+HEADERS = BASE_HEADERS + SESSION_ID + '\''
+
+GET_USERS_BODY =  '''--data-binary '{"body":[{"message_type":81,"server_get_encounters":{"number":10,"context":1,"user_field_filter":{"projection":[210,370,200,230,490,540,530,560,291,732,890,930,662,570,380,493,1140,1150,1160,1161],"request_albums":[{"album_type":7},{"album_type":12,"external_provider":12,"count":8}],"game_mode":0,"request_music_services":{"top_artists_limit":8,"supported_services":[29],"preview_image_size":{"width":120,"height":120}}}}}],"message_id":7,"message_type":81,"version":1,"is_background":false}' --compressed > ''' + USERS_FILE_NAME
+
+LIKE_VOTE = 2
+PASS_VOTE = 3
+
+MIN_AGE = 24
+MAX_AGE = 47
+
+MAX_DISTANCE = 15.0
+KIDS_PHRASES = [' ילד ', ' ילדה ', ' + ', ' פלוס ', ' אמא ', ' נסיך ', ' נסיכה ']
