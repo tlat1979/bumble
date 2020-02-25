f4ec8c1e285a27177788a6d4037050d04cf157a3
Tal Tamir <tal.tamir@cyesec.com>
2020-02-25 14:45:05 +0200

    Added users entity to FB script

--- fb.js
+++ fb.js
@@ -1,7 +1,6 @@
 var getUrl = async url => {
-    var response = await fetch(url)
-    var data = await response.text()
-    console.log(data)
+    var response = await fetch(url);
+    return await response.text();
 }
 
 // USING PAGE: https://m.facebook.com/friends/center/suggestions
@@ -14,12 +13,29 @@ var main = async () => {
         behavior: 'smooth'
     });
 
+    let user = {}
 
-    var friends = document.querySelectorAll("[data-sigil='undoable-action'] h3 a, [data-sigil='undoable-action'] h1 a");
 
-    friends.forEach(async f => {
-        let url = f.href.split('?');
-        console.log(f.text + " " + url[0]);
-        //await getUrl(url[0]+"/about?section=relationship")
-    });
+    let friends = document.querySelectorAll("[data-sigil='undoable-action'] h3 a, [data-sigil='undoable-action'] h1 a");
+    user.name = friends[0].text;
+    user.url = friends[0].href.split('?')[0];
+
+    let userAboutPage = await getUrl(user.url + "/about");
+    let AboutPageDomParser = new DOMParser();
+    user.AboutPageDoc = AboutPageDomParser.parseFromString(userAboutPage, "text/html");
+
+
+    console.log(1);
+
+    //     friends.forEach(async f => {
+    //         let url = f.href.split('?');
+    //         console.log(f.text + " " + url[0]);
+
+    //         //$$("[data-sigil='profile-card']")
+
+
+    //         var res = await getUrl(url[0]+"/about")
+    //         console.log(res);
+    //         //https://m.facebook.com/profile.php?v=info&id=100006567487010
+    //     });
 }
