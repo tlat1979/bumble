e5b3abd41d5bb9c1c86970bf8b125225121d3687
Tal Tamir <tal.tamir@cyesec.com>
2020-02-25 16:11:05 +0200

    Get user photos

--- fb.js
+++ fb.js
@@ -7,22 +7,33 @@ var getUrl = async url => {
 
 var main = async () => {
 
-    window.scroll({
-        top: 4000,
-        left: 0,
-        behavior: 'smooth'
-    });
+    // load more users
+    window.scroll({ top: 4000, left: 0, behavior: 'smooth' });
 
     let user = {}
-
-
     let friends = document.querySelectorAll("[data-sigil='undoable-action'] h3 a, [data-sigil='undoable-action'] h1 a");
     user.name = friends[0].text;
     user.url = friends[0].href.split('?')[0];
 
     let userAboutPage = await getUrl(user.url + "/about");
-    let AboutPageDomParser = new DOMParser();
-    user.AboutPageDoc = AboutPageDomParser.parseFromString(userAboutPage, "text/html");
+    let aboutPageDomParser = new DOMParser();
+    user.aboutPageDoc = aboutPageDomParser.parseFromString(userAboutPage, "text/html");
+
+    user.aboutSections = user.aboutPageDoc.querySelectorAll("[data-sigil='profile-card']");
+
+    user.aboutSections.forEach(section => {
+        section.id != "" ?
+            user[section.id] = section.textContent :
+            user[section.textContent] = section.textContent ;
+
+        if (section.textContent == "Photos") {
+            user.photos = []
+
+            let temp = section.querySelectorAll("a i");
+            temp.forEach(t => user.photos.push(t.style.background.split('"')[1])) //[3].style.background.split('"');
+            console.log(9);
+        }
+    });
 
 
     console.log(1);
