31f8e84217bf0082b6c8091b63e59d82b1e449a0
Tal Tamir <tal.tamir@safebreach.com>
2020-02-28 12:33:02 +0200

    Updated the "living" field

--- fb.js
+++ fb.js
@@ -36,74 +36,74 @@ var UTILS = {
 
 var USERS = {
 
-        getSuggestions: () => document.querySelectorAll("[data-sigil='undoable-action'] h3 a, [data-sigil='undoable-action'] h1 a"),
+    getSuggestions: () => document.querySelectorAll("[data-sigil='undoable-action'] h3 a, [data-sigil='undoable-action'] h1 a"),
 
-        parseSuggestions: async suggestions => {
-            parsedSuggestions = [];
+    parseSuggestions: async suggestions => {
+        parsedSuggestions = [];
 
-            for await (i of Array.from(Array(3).keys())) {
-                let user = {};
+        for await (i of Array.from(Array(3).keys())) {
+            let user = {};
 
-                user.name = suggestions[i].text;
-                user.url = user.url = suggestions[i].href.split('?')[0];
+            user.name = suggestions[i].text;
+            user.url = user.url = suggestions[i].href.split('?')[0];
 
-                user.aboutPageDoc = await UTILS.getUrl(user.url + "/about");
-                user.aboutSections = user.aboutPageDoc.querySelectorAll("[data-sigil='profile-card']");
-                USERS.parseUserSections(user);
-                parsedSuggestions[i] = user;
+            user.aboutPageDoc = await UTILS.getUrl(user.url + "/about");
+            user.aboutSections = user.aboutPageDoc.querySelectorAll("[data-sigil='profile-card']");
+            USERS.parseUserSections(user);
+            parsedSuggestions[i] = user;
 
-                await UTILS.sleep(UTILS.getRandomWait());
+            await UTILS.sleep(UTILS.getRandomWait());
 
+        }
+        return parsedSuggestions;
+    },
+
+    parseUserSections: async user => {
+
+        user.aboutSections.forEach(section => {
+
+            if (section.id != "") {
+                switch (section.id) {
+                    case "family":
+                        user.family = section.querySelectorAll("a");
+                        break;
+                    case "relationship":
+                        user.relationship = section.querySelectorAll("._52ja._5cds._5cdt")[0].textContent;
+                        break;
+                    case "basic-info":
+                        user["basic-info"] = section.querySelectorAll("._5cdv")[0].textContent;
+                        break;
+                    case "work":
+                        user.work = section.querySelectorAll("a")[1].text;
+                        break;
+                    case "education":
+                        user.education = section.querySelectorAll("a")[1].text;
+                        break;
+                    case "Videos":
+                        user.Videos = []
+                        break;
+                    case "Photos":
+                        user.Photos = []
+                        var temp = section.querySelectorAll("a i");
+                        temp.forEach(t => user.Photos.push(t.style.background.split('"')[1]));
+                        break;
+                    case "living":
+                        user.living = [];
+                        var temp = section.querySelectorAll("h4");
+                        temp.forEach(c => user.living.push(c.textContent));
+                        break;
+                }
             }
-            return parsedSuggestions;
-        },
 
-        parseUserSections: async user => {
+            else {
+                let mutualFriends = section.querySelectorAll("strong");
+                if (mutualFriends && mutualFriends.length > 0) {
+                    user.mutualFriends = [];
+                    mutualFriends.forEach(f => user.mutualFriends.push(f.textContent));
+                }
+            }
 
-            user.aboutSections.forEach(section => {
-
-                    if (section.id != "") {
-                        switch (section.id) {
-                            case "family":
-                                user.family = section.querySelectorAll("a");
-                                break;
-                            case "relationship":
-                                user.relationship = section.querySelectorAll("._52ja._5cds._5cdt")[0].textContent;
-                                break;
-                            case "basic-info":
-                                user["basic-info"] = section.querySelectorAll("._5cdv")[0].textContent;
-                                break;
-                            case "work":
-                                user.work = section.querySelectorAll("a")[1].text;
-                                break;
-                            case "education":
-                                user.education = section.querySelectorAll("a")[1].text;
-                                break;
-                            case "Videos":
-                                user.Videos = []
-                                break;
-                            case "Photos":
-                                user.Photos = []
-                                var temp = section.querySelectorAll("a i");
-                                temp.forEach(t => user.Photos.push(t.style.background.split('"')[1]));
-                                break;
-                            case "living":
-                                user.living = [];
-                                var temp = section.querySelectorAll("a");
-                                temp.forEach(c => user.living.push(c.text));
-                                break;
-                        }
-                    }
-
-                    else {
-                        let mutualFriends = section.querySelectorAll("strong");
-                        if (mutualFriends && mutualFriends.length > 0) {
-                            user.mutualFriends = [];
-                            mutualFriends.forEach(f => user.mutualFriends.push(f.textContent));
-                        }
-                    }
-                
-            });
+        });
     },
 }
 
