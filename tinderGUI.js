9ccfb69385dc396c709fcfec00072fb0ec763e3a
Tal Tamir <tal.tamir@safebreach.com>
2020-03-03 14:19:32 +0200

    Tinder UI - collecting info

--- tinderGUI.js
+++ tinderGUI.js
@@ -1,55 +1,70 @@
-// $$("[aria-label='Like']")[0].click()
-// $$("[aria-label='Nope']")[0].click()
-
-
-
-
-
-
 class User {
-    constructor(options) {
-        this.name = options.name || "N/A";
-        this.age = options.age || 0;
-        this.about = options.about || "N/A";
-    };
 
+    constructor() {
+        this.MAX_AGE = 47;
+        this.MIN_AGE = 24
+        this.MAX_DISTANCE = 21;
+        this.BROKEN_CONDITIONS = ["+", "ילד", "אמא", "mom", "נסיך", "נסיכה", "פלוס"];
 
-    printUser() {
-        console.log("Name: " + this.name + " Age: " + this.age + " About: " + this.about);
-    };
+    }
+
+    getUserDetails = () => {
+        try {
+            this.users = $$(".recCard__info")[1] || [];
+            this.name = this.users.children[0].children[0].children[0].textContent || "";
+            this.age = this.users.children[0].children[0].children[1].textContent || 0;
+            this.about = this.users.children[1].textContent || "";
+            this.distance = $$(".Row").length > 0 ? $$(".Row")[0].textContent : 0;
+            this.moreInfo = $$(".Row").length > 1 ? $$(".Row")[1].textContent : "";
+            return true;
+        }
+        catch (e) {
+            console.warn(e);
+            return false;
+        }
+    }
+
+    isValidUser = () => {
+        let validAge = true;
+        let validDistance = true;
+        let validAbout = true;
+
+        if (this.user.age) {
+            validAge = this.age < this.MAX_AGE && this.age > this.MIN_AGE;
+        }
+        if (this.user.distance) {
+            validDistance = this.distance < this.MAX_DISTANCE;
+        }
+        if (this.about) {
+            for (let i in this.BROKEN_CONDITIONS) {
+                if ((this.user.about).includes(this.BROKEN_CONDITIONS[i])) {
+                    validAbout = false;
+                    break;
+                }
+            }
+        }
+        let details = "Name " + this.user.name + " | Age " + this.user.age + " | Distance " + this.user.distance + " | About " + this.user.about;
+        validAge && validDistance && validAbout ?
+            log("Liked: " + details, "green") :
+            log("Passed: " + details, "red");
+        return validAge && validDistance && validAbout;
+    }
+
+    printUser = () => {
+        console.log(
+            "Name: " + this.name + " | ",
+            "Age: " + this.age + " | ",
+            "About: " + this.about + " | ",
+            "Distance " + this.distance + " | ",
+            "Info " + this.moreInfo + " | "
+        );
+    }
 }
 
 var main = () => {
     var users, name, age, about = '';
 
-    try {
-        users = $$(".recCard__info");
-        name = users[1].children[0].children[0].children[0].textContent;
-        age = users[1].children[0].children[0].children[1].textContent;
-        about = users[1].children[1].textContent
-    }
-    catch (e) {
-        console.warn(e);
-        return;
-    }
-
-    var user = new User({
-        name: name,
-        age: age,
-        about: about
-    });
-
-    user.printUser();
-
-
-
-    //currentUser = new User();
-
-    // users.forEach(user => console.log(user.textContent))
-
-    // a[0] next user
-    // a[1] current user
-    // a[2] prev user
-
-
+    var user = new User();
+    if (user.getUserDetails())
+        user.printUser();
 } 
