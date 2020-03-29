7b301cb53d31d714615c4677f834d7197540384f
Tal Tamir <tal.tamir@safebreach.com>
2020-03-29 14:45:48 +0300

    Added main fo ok gui

--- OKGui.js
+++ OKGui.js
@@ -1,29 +1,41 @@
+// General sleep function
 var sleep = ms => new Promise(resolve => setTimeout(resolve, ms));
+
+// This function assumes that it rund of the doubleTake page 
 var getUserDetails = async userId => {
     var user = {};
+
+    // User Photos
+    user.photos = [];
+    var userPhotos = window.document.querySelectorAll(".qmcard-contents .image_wrapper > img");
+    for (photo of userPhotos) {
+        user.photos.push(photo.src);
+    }
+
+    // Open a new window for rendering the user profile
     var win = window.open("https://www.okcupid.com/profile/" + userId + "?cf=quickmatch", "xxxx", "height=200,width=200");
     await sleep(10000);
 
+    // User basic info    
     user.details = [];
-    var userDetails = win.document.querySelectorAll(".matchprofile-details-section > .matchprofile-details-text");
+    var userDetails = win.document.querySelectorAll(".matchprofile-details-section");
     for (details of userDetails) {
         user.details.push(details.textContent);
     }
 
+    // User extended info (essays)
     var profileEssays = win.document.querySelectorAll(".profile-essay");
     user.essays = [];
     for (essay of profileEssays) {
-        var header = essay.querySelectorAll("h2");
-        var body = essay.querySelectorAll("p");
-        user.essays.push({ [header[0].textContent + " " + header[1].textContent]: body[0].textContent });
+        user.essays.push(essay.textContent);
     }
 
-    console.log(user);
     win.close();
 }
 
-
-
-getUserDetails("7340335320965521072");
+var main = async () => {
+    var user = await getUserDetails("7340335320965521072");
+    console.log(user);
+}
 
 
